46c6a706a1de50f9c92ba4c2051fbc8ac9492cbf
Denilson4554 <deni1432014@gmail.com>
2025-05-12 09:47:33 -0600

    Add files via upload

---
+++ codigo_ascii.cpp
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(){
+    char letra = ' ';
+    
+    printf("Ingresa una letra: ");
+    scanf(" %c", &letra);
+    printf("El codigo ascii es %d\n", letra);
+
+    return 0;
+}
